a24e776248ae225f97c5b58fcb3629fb66c903a3
sávio <saviopf22@gmail.com>
2019-04-02 17:18:01 -0300

    aprendendo exports com input

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
 // input com nodejs pela linha de comando
 
 const readline = require('readline')
-
+/*
 const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -19,4 +19,17 @@ input.question('seu nome~> ', (nome) => {
 input.question('seu sobrenome~> ', (sobrenome) => {
     console.log(`seu sobrenome~> ${sobrenome}`)
     input.close()
+})*/
+
+
+const input1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
+ 
+input1.question('> ', (data) => {
+    console.log(data)
+    input1.close()
+})
+
+module.exports.entrada = input1.question
---
+++ testExports.js
@@ -0,0 +1,3 @@
+const input = require('./input')
+
+input.entrada
