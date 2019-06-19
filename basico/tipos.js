913249b69ef9b0468b16384a473d28d13048c0c4
Savio <saviopf22@gmail.com>
2019-06-19 10:12:50 -0300

    revisando os tipos do javascript

---
+++ basico/tipos.js
@@ -0,0 +1,22 @@
+const bool = false
+const str = 'iae'
+const int = 1
+const float = 1.1
+const array = [1, 2, 3, 'iae', [1, 2, 3]]
+const obj = { nome: 'sávio', idade: 20, peso: 64.5, aprovado: true }
+
+console.log(array)
+console.log(obj)
+
+console.log()
+
+console.log(array[0])
+console.log(obj.nome)
+
+console.log()
+
+const num1 = 10
+const num2 = '10'
+
+console.log(num1 + num1)
+console.log(num1 + num2)
