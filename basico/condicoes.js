f7540d29bd0086895ccaa1b8badce3c976d0db7a
Savio <saviopf22@gmail.com>
2019-06-19 10:57:30 -0300

    testes com condiçoes

---
+++ basico/condicoes.js
@@ -0,0 +1,88 @@
+const num1 = 1
+const num2 = '1'
+
+// somente '==' ou '!=' verifica somente o valor  
+// com '===' ou '!==' verifica o valor e tipo
+
+if(num1 == '1'){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num1 === '1'){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num2 == 1){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num2 === 1){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num1 != '1'){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num1 !== '1'){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num2 != 1){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num2 !== 1){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+const num3 = 5
+const num4 = '10'
+
+if(num3 > num4){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
+
+if(num3 < num4){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+console.log()
