e1b5a7dd0caa0e6f0f765537bae37979af970a9a
savio <saviopf22@gmail.com>
2020-02-02 02:19:56 -0300

    outra forma para funcionar em mais navegadores no padrão brasileiro

--- mascaras/masks.js
+++ mascaras/masks.js
@@ -4,9 +4,10 @@ const maskPhone = (phone) => {
   return phone
     .replace(/\D/g, '')
     .replace(/(\d{2})(\d)/, '($1) $2')
-    .replace(/(\d{4})(\d)/, '$1\f$2')
-    .replace(/(\d{4})\f(\d)(\d{4})/, '$1$2 $3')
-    .replace(/(\d{4})\d+?$/, '$1')
+    .replace(/(\d{5})(\d)/, '$1 $2')
+    // work only in opera
+    //.replace(/(\d{4})\f(\d)(\d{4})/, '$1$2 $3')
+    //.replace(/(\ \d{4})\d+?$/, '$1')
 }
 
 phoneInput.addEventListener('input', (event) => {
