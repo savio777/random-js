e81a3f5986e3e4a940904f6eef3066e487e0e5c7
savio <saviopf22@gmail.com>
2021-05-18 22:42:52 -0300

    mascara de data e melhor organização de codigo

--- mascaras/masks.js
+++ mascaras/masks.js
@@ -1,80 +1,110 @@
-const phoneInput = document.getElementById('phone')
+const phoneInput = document.getElementById("phone");
 
 // (99) 99999 9999
 const maskPhone = (phone) => {
   return phone
-    .replace(/\D/g, '')
-    .replace(/(\d{2})(\d)/, '($1) $2')
-    .replace(/(\d{5})(\d)/, '$1 $2')
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{5})(\d)/, "$1 $2");
   // work only in opera
   //.replace(/(\d{4})\f(\d)(\d{4})/, '$1$2 $3')
   //.replace(/(\ \d{4})\d+?$/, '$1')
-}
+};
 
-phoneInput.addEventListener('input', (event) => {
-  event.target.value = maskPhone(event.target.value)
-}, false)
+phoneInput.addEventListener(
+  "input",
+  (event) => {
+    event.target.value = maskPhone(event.target.value);
+  },
+  false
+);
 
-
-const cpfInput = document.getElementById('cpf')
+const cpfInput = document.getElementById("cpf");
 
 // 999.999.999-99
 const maskCpf = (cpf) => {
   return cpf
-    .replace(/\D/g, '')
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d)/, '$1-$2')
-    .replace(/(-\d{2})(\d)/, '$1')
-}
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1-$2")
+    .replace(/(-\d{2})(\d)/, "$1");
+};
 
-cpfInput.addEventListener('input', (event) => {
-  event.target.value = maskCpf(event.target.value)
-}, false)
+cpfInput.addEventListener(
+  "input",
+  (event) => {
+    event.target.value = maskCpf(event.target.value);
+  },
+  false
+);
 
-
-const cnpjInput = document.getElementById('cnpj')
+const cnpjInput = document.getElementById("cnpj");
 
 // 99.999.999/9999-99
 const maskCnpj = (cnpj) => {
   return cnpj
-    .replace(/\D/g, '')
-    .replace(/(\d{2})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d)/, '$1/$2')
-    .replace(/(\d{4})(\d)/, '$1-$2')
-    .replace(/(-\d{2})(\d)/, '$1')
-}
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(-\d{2})(\d)/, "$1");
+};
 
-cnpjInput.addEventListener('input', (event) => {
-  event.target.value = maskCnpj(event.target.value)
-}, false)
+cnpjInput.addEventListener(
+  "input",
+  (event) => {
+    event.target.value = maskCnpj(event.target.value);
+  },
+  false
+);
 
-
-const priceInput = document.getElementById('price')
+const priceInput = document.getElementById("price");
 
 // 999.999.999,99
 const maskPrice = (price) => {
-  let newPrice = ''
+  let newPrice = "";
 
   if (price.length === 1) {
-    newPrice = '0.0' + price
+    newPrice = "0.0" + price;
   } else {
-
     newPrice = price
-      .replace('R$', '')
-      .replace(/\s/g, '')
-      .replace('.', '')
-      .replace(',', '.')
+      .replace("R$", "")
+      .replace(/\s/g, "")
+      .replace(".", "")
+      .replace(",", ".");
   }
 
-  console.log('sem letras~> ', newPrice)
+  console.log("sem letras~> ", newPrice);
 
-  const value = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(newPrice)
-  console.log('res~> ', value)
-  return value
-}
+  const value = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(newPrice);
+  console.log("res~> ", value);
+  return value;
+};
 
-priceInput.addEventListener('input', (event) => {
-  event.target.value = maskPrice(event.target.value)
-})
+priceInput.addEventListener("input", (event) => {
+  event.target.value = maskPrice(event.target.value);
+});
+
+// 08/11/1997
+const maskDate = (date) => {
+  return date
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d)/, "$1");
+};
+
+const dateInput = document.getElementById("date");
+
+dateInput.addEventListener(
+  "input",
+  (event) => {
+    event.target.value = maskDate(event.target.value);
+  },
+  false
+);
