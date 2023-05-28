71b6988e44f72bc94e97cece24ad73dc87fca6b9
Sávio <saviopf22@gmail.com>
2023-05-28 11:04:25 -0300

    add rest params

--- es6.js
+++ es6.js
@@ -4,25 +4,25 @@
  */
 
 // es5 (antigo)
-var somar5 = function(x, y){
-    return x + y
-}
+var somar5 = function (x, y) {
+  return x + y;
+};
 
 /* es6 (atual e recomendado)
-*  ~> utilizar const em funções para não ser possivel modificar o valor da variavel sem passar parametros
-*   ex: somar6 = 1  -> isso irá gerar erro
-*  ~> não usar function e em vez de disso utilizar =>
-*/ 
+ *  ~> utilizar const em funções para não ser possivel modificar o valor da variavel sem passar parametros
+ *   ex: somar6 = 1  -> isso irá gerar erro
+ *  ~> não usar function e em vez de disso utilizar =>
+ */
 
 const somar6 = (x, y) => {
-    return x + y 
-}
+  return x + y;
+};
 
 // como a função só tem uma linha pode tirar {} e a palavra return
 //const somarSimplificado = (x, y) => x + y
 
 // problemas q podem ser gerados por metodos antigos sem const
-somar5 = 2
+somar5 = 2;
 
 // ERROS
 // TypeError: Assignment to constant variable.
@@ -39,19 +39,25 @@ somar5 = 2
 //console.log(`${somar5}, ${somar6(1, 1)}`)
 
 /* segunda parte
-** uso do HIGHER-ORDER FUNCTION
-**/
+ ** uso do HIGHER-ORDER FUNCTION
+ **/
 
-const mult = (x, y) => x * y
+const mult = (x, y) => x * y;
 
 // função para chamar outras funções de calculo, e já passa os parametros
-const calcular = (fn, x, y) => fn(x, y)
+const calcular = (fn, x, y) => fn(x, y);
 
-console.log(calcular(somar6, 1, 1))  // 2
-console.log(calcular(mult, 1, 1))    // 1
+console.log(calcular(somar6, 1, 1)); // 2
+console.log(calcular(mult, 1, 1)); // 1
 
-// CURRYING 
-const somarSimplificado = x => y => x + y
-const somarDeNovo = somarSimplificado(1)
+// CURRYING
+const somarSimplificado = (x) => (y) => x + y;
+const somarDeNovo = somarSimplificado(1);
 
-console.log(somarDeNovo(1))  // 2
+console.log(somarDeNovo(1)); // 2
+
+// https://www.youtube.com/watch?v=nZ1DMMsyVyI
+
+const add = (...nums) => nums.reduce((x, y) => x + y);
+
+console.log(add(1, 2, 3, 6, 1, 5, 8, 8));
